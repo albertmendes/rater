57da98c1353bb3d7af1a61f4b274417ac4624d2a
Albert Mendes <_@amendes.me>
2018-04-04 22:21:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+    <div class="rating-wrapper" id="rating1">
+      <label id="rate-string"></label>
+      <ul>
+        <li class="r1 star"></li>
+        <li class="r2 star"></li>
+        <li class="r3 star"></li>
+        <li class="r4 star"></li>
+        <li class="r5 star"></li>
+      </ul>
+    </div>
+
+  <script src="js/main.js" data-id="rating1"></script>
+  </body>
+</html>
